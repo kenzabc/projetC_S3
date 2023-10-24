15640b27310571ad1705b0925cf532b2ae1990fc
Kenza <kenza.braham-chaouch@efrei.net>
2023-10-24 17:04:07 +0200

    ok

---
+++ set.c
@@ -0,0 +1,9 @@
+//
+// Created by kenza on 24/10/2023.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+#include "set.h"
---
+++ set.h
@@ -0,0 +1,22 @@
+//
+// Created by kenza on 24/10/2023.
+//
+
+#ifndef PROJETC_S3_SET_H
+#define PROJETC_S3_SET_H
+
+typedef struct s_d_cell
+{
+    int value;
+    struct s_d_cell *next_0;
+    struct s_d_cell *next_1;
+} t_d_cell;
+
+typedef struct s_d_list
+{
+    t_d_cell *head_0;
+    t_d_cell *head_1;
+} t_d_list;
+
+typedef
+#endif //PROJETC_S3_SET_H
